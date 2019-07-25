cf829580edee75bbdef4ea335af550fc1616151b
Andrew Muwakanya <myandrewz@gmail.com>
2019-07-25 17:21:39 +0300

    chore : moved header and footer to the sharedd folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core'; 
-//import { HttpModule } from '@angular/http'
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
-//import {QuillModule} from 'ngx-quill';
-//import { HttpClient, HttpHandler } from '@angular/common/http';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
-import { LoginComponent } from './login/login.component'; 
+import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
 import { RegisterComponent } from './register/register.component';
 import { UserResolver } from './user/user.resolver';
-import { AuthGuard } from './core/auth.guard'; 
+import { AuthGuard } from './core/auth.guard';
 import { AuthService } from './core/auth.service';
 import { UserService } from './core/user.service';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
@@ -21,12 +19,10 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NewProfileComponent } from './new-profile/new-profile.component';
 import {SuiModule} from 'ng2-semantic-ui';
-import { NewStudentComponent } from './new-student/new-student.component'; 
+import { NewStudentComponent } from './new-student/new-student.component';
 import { NewMentorComponent } from './new-mentor/new-mentor.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StudentUserComponent } from './student-user/student-user.component';
-
-//import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 import { MentorComponent } from './mentor/mentor.component';
 import { CareerJobsComponent } from './career-jobs/career-jobs.component';
 import { BlogsComponent } from './blogs/blogs.component';
@@ -34,26 +30,15 @@ import { ImagesVideosComponent } from './images-videos/images-videos.component';
 import { ReportsComponent } from './reports/reports.component';
 import { OverviewComponent } from './overview/overview.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MaterialModule} from './material'
+import { MaterialModule} from './material';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { ToastrModule } from 'ngx-toastr';
-//import { FlexLayoutModule } from '@angular/flex-layout';
-import { HashLocationStrategy } from "@angular/common";
-//import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
-import { Location } from "@angular/common";
-import { LocationStrategy } from "@angular/common";
 import { DialogComponent } from './dialog/dialog.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import {WysiwygComponent} from './wysiwyg/wysiwyg.component';
-import { FooterComponent } from './footer/footer.component';
-import { SharedModule } from './shared/shared.module';
+import { FooterComponent } from './shared/footer/footer.component';
 import { PostsModule } from './posts/posts.module';
-//import { DialogComponent } from './dialog/dialog.component';
-const routes : Routes = [
-  {path : '',redirectTo: '/blog', pathMatch: 'full' },
-  {path : '',loadChildren: './posts/posts.module#PostModule' },
-  {path : '',redirectTo: '/blog', pathMatch: 'full' },
-]
+
 import { HomeComponent } from './home/home.component';
 import { CareerEventsComponent } from './career-events/career-events.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
@@ -64,8 +49,7 @@ import { SearchfilterPipe } from './pipes/searchfilter.pipe';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { NewsletterComponent } from './newsletter/newsletter.component';
 import {MatInputModule} from '@angular/material';
-//import { DialogComponent } from './dialog/dialog.component';
-//import GoogleMaps
+
 import { HttpClientModule } from '@angular/common/http';
 import { AgmCoreModule } from '@agm/core';
 import { InternshipComponent } from './internship/internship.component';
@@ -134,8 +118,6 @@ import { CategoriesComponent } from './categories/categories.component';
    // HttpModule,
     ReactiveFormsModule,
     MaterialModule,
-    //RichTextEditorAllModule,
-   // FroalaEditorModule.forRoot(), FroalaViewModule.forRoot(),
     FormsModule,
     RouterModule,
     HttpClientModule,
@@ -145,7 +127,7 @@ import { CategoriesComponent } from './categories/categories.component';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCi3hSfPY4V_5h4XIBuAv13P7AQlwvIG6A'
     }),
-  
+
 
     RouterModule.forRoot(rootRouterConfig, { useHash: false }),
     AngularFireModule.initializeApp(environment.firebase),
@@ -157,24 +139,16 @@ import { CategoriesComponent } from './categories/categories.component';
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       timeOut: 10000,
-      //positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
-    SharedModule,
+
     PostsModule,
-   // QuillModule.forRoot()
-   // FlexLayoutModule
+
   ],
-  
+
   providers: [AuthService, UserService, UserResolver,
-    
-                      
-       
-    Location,
-		{
-			provide: LocationStrategy,
-			useClass: HashLocationStrategy
-},
+
+
     AuthGuard],
   bootstrap: [AppComponent]
 })
